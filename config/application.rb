10f536ba6dd571c204ccbea8280281220712d461
Cheryl Brown <sousou617@gmail.com>
2019-04-06 20:52:36 +0000

    remove web-console

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,6 @@ module Suggestotron
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     #
-    config.web_console.whitelisted_ips = '72.246.0.0/24'
+    #config.web_console.whitelisted_ips = '72.246.0.0/24'
   end
 end
