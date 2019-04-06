e1d46f0b3ed975f4c5884defdbad4205e5ee28bf
EC2 Default User <ec2-user@ip-172-31-10-23.us-east-2.compute.internal>
2019-04-06 20:27:54 +0000

    initial commit

---
+++ config/application.rb
@@ -0,0 +1,17 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Suggestotron
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    #
+    config.web_console.whitelisted_ips = '72.246.0.0/24'
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :topics do
+  member do
+    post 'upvote'
+  end
+end
+    root 'topics#index'
+end
+
